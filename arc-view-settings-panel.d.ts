764aa26b41dc1e2e4a653a6f5d64e902596eb076
Pawel <jarrodek@gmail.com>
2019-01-29 00:28:08 -0800

    Update: Updating polymer docs

--- arc-view-settings-panel.d.ts
+++ arc-view-settings-panel.d.ts
@@ -8,8 +8,13 @@
  *   arc-view-settings-panel.html
  */
 
+
+// tslint:disable:variable-name Describing an API that's defined elsewhere.
+// tslint:disable:no-any describes the API as best we are able today
+
 /// <reference path="../polymer/types/polymer-element.d.ts" />
 /// <reference path="../arc-settings-panel-mixin/arc-settings-panel-mixin.d.ts" />
+/// <reference path="../arc-settings-panel-mixin/arc-settings-panel-styles.d.ts" />
 /// <reference path="../paper-dropdown-menu/paper-dropdown-menu.d.ts" />
 /// <reference path="../paper-listbox/paper-listbox.d.ts" />
 /// <reference path="../paper-item/paper-item.d.ts" />
@@ -33,6 +38,10 @@ declare namespace UiElements {
    * Custom property | Description | Default
    * ----------------|-------------|----------
    * `--arc-view-settings-panel` | Mixin applied to this elment | `{}`
+   * `--arc-settings-panel-header-color` | Color of the settings panel header | `--accent-color`
+   * `--arc-settings-panel-icon-color` | Settings panel icon color | `rgba(0, 0, 0, 0.34)`
+   * `--arc-font-body1` | Mixin apllied to the description elements | `{}`
+   * `--arc-font-subhead` | Mixin applied to the section headers | `{}`
    */
   class ArcViewSettingsPanel extends
     ArcComponents.ArcSettingsPanelMixin(
